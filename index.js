bc0c632185ec871af5849364a98762e691ca66fd
Safaemqt <139119530+Safaemqt@users.noreply.github.com>
2023-07-31 17:59:28 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let myName = "safae"
+
+console.log(myName)
+let age = 5
+let sport = ["basketball" , "football", "handball", "volleyball"]
+let person= {name: 'Safae', age:20 ,country: 'Morocco'}
+console.log('my age is ', age)
+console.log(sport)
